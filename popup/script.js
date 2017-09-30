817384bffb82ebe3020bb5c3bf267faf99efe958
codlocker <ipsit.sahoo11@gmail.com>
2017-10-01 03:54:15 +0530

    Version 1.0 Complete. Added Search Bar. Improvement on UI Neededgit status

--- popup/script.js
+++ popup/script.js
@@ -1,28 +1,60 @@
+document.addEventListener('DOMContentLoaded', function() {
+    let link = document.getElementById('sub_button');
+    // onClick's logic below:
+    link.addEventListener('click', function() {
+        formLink();
+    });
+});
+
 $(document).ready(function () {
+    executeScript("http://www.geeksforgeeks.org/");
+});
+
+function executeScript(link) {
     $.ajax({
-        url: "http://www.geeksforgeeks.org/",
+        url: link,
         type: "GET",
         success: function (response) {
             let s = $.parseHTML(response);
             let context = $("#content");
             context.html(s);
-            let all_h2s = $("#content .entry-title").text().split(" \n\t");
+            let entry_point;
+            entry_point = context.find(".entry-title");
             let array = [];
-            for(i = 0; i < all_h2s.length; i++ )
+            for(i = 0; i < entry_point.length; i++ )
             {
                 let h1_nw = document.createElement("h3");
                 let a_nw = document.createElement("a");
-                let all_as = $("header a").attr("href").split(" \n\t");
-                $(a_nw).attr("href", all_as);
                 $(h1_nw).append(a_nw);
-                $(a_nw).text(all_h2s[i]);
+                $(a_nw).html($(entry_point).get(i));
                 $(h1_nw).addClass("content_data");
-                console.log(h1_nw);
                 $("#data").append(h1_nw);
             }
         },
         error: function () {
-          alert("Error!!");
+            $("#data").html("<p class='error'>Error: Page Does Not Exist!</p>");
+            $("#content").html("");
         }
     });
-});
+}
+
+function formLink() {
+    let data = $("[name='url_form']").val();
+    let inp_data = parseInt(data);
+    const url = "http://www.geeksforgeeks.org/";
+    if (inp_data === 1)
+    {
+        executeScript(url);
+    }
+    else if(inp_data > 1)
+    {
+        let new_url = url + "page/" + String(inp_data);
+        console.log(new_url);
+        $("#data").html("");
+        $("#content").html("");
+        executeScript(new_url);
+    }
+    else {
+        alert("Error");
+    }
+}
